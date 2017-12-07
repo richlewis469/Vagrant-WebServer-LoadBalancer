d0b12aaedd9141b70314ef5984785b5cf86c26dc
Rich Lewis <rich.lewis@oracle.com>
2017-12-07 08:50:51 -0600

    Update

--- vagrant-shell/provision.sh
+++ vagrant-shell/provision.sh
@@ -2,7 +2,21 @@
 
 # Enable provisioning of the client with a shell script.
 echo "Entering Shell Provisoning"
-yum -q -y makecache fast
+
+yum makecache fast
+yum install deltarpm --assumeyes
+yum install wget --assumeyes
+
+/bin/mv /etc/yum.repos.d/public-yum-ol7.repo /etc/yum.repos.d/public-yum-ol7.repo.orig
+wget -q http://yum.oracle.com/public-yum-ol7.repo -P /etc/yum.repos.d
+
+yum install yum-utils --assumeyes
+yum-config-manager --enable ol7_addons
+yum-config-manager --enable ol7_software_collections
+yum-config-manager --enable ol7_developer
+yum-config-manager --enable ol7_developer_EPEL
+
+yum makecache fast
 
 echo " "
 ip addr show
